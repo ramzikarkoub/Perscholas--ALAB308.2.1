1305f49d5333785f8bdccb53ba932aa16f393782
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-11-25 12:25:43 -0500

    calculated the max number of plants

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const PI = 3.1415;
 const radius = 5;
 const area = PI * radius * radius;
 console.log("Garden Area:", area);
+
+// the max number of plants
+const plantSpace = 0.8;
+const maxPlants = Math.floor(area / plantSpace);
+console.log("Maximum number of plants:", maxPlants);
