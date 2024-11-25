479e8b470e6d41811faf79c40483ca7748b1213c
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-11-25 12:40:12 -0500

    Predict the plant growth

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ console.log("Garden Area:", area);
 const plantSpace = 0.8;
 const maxPlants = Math.floor(area / plantSpace);
 console.log("Maximum number of plants:", maxPlants);
+
+// Predict the plant growth after a specific number of weeks.
+
+const predictGrowth = (weeks) => {
+  let plants = 20;
+  for (let i = 0; i < weeks; i++) {
+    plants *= 2;
+  }
+  console.log(plants);
+  return plants;
+};
+
+predictGrowth(5);
